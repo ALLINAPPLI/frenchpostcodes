f986745270414687c854fb61874c2d9066efa8da
Dewy <dewymercerais@gmail.com>
2024-09-25 17:51:20 +0200

    civix upgrade sur l'extension

--- frenchcodepostaux.php
+++ frenchcodepostaux.php
@@ -15,15 +15,6 @@ function frenchcodepostaux_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function frenchcodepostaux_civicrm_xmlMenu(&$files) {
-  _frenchcodepostaux_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -33,96 +24,12 @@ function frenchcodepostaux_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function frenchcodepostaux_civicrm_postInstall() {
-  _frenchcodepostaux_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function frenchcodepostaux_civicrm_uninstall() {
-  _frenchcodepostaux_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function frenchcodepostaux_civicrm_enable() {
   _frenchcodepostaux_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function frenchcodepostaux_civicrm_disable() {
-  _frenchcodepostaux_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function frenchcodepostaux_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _frenchcodepostaux_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function frenchcodepostaux_civicrm_managed(&$entities) {
-  _frenchcodepostaux_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function frenchcodepostaux_civicrm_caseTypes(&$caseTypes) {
-  _frenchcodepostaux_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function frenchcodepostaux_civicrm_angularModules(&$angularModules) {
-  _frenchcodepostaux_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function frenchcodepostaux_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _frenchcodepostaux_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -136,12 +43,7 @@ function frenchcodepostaux_civicrm_alterSettingsFolders(&$metaDataFolders = NULL
 //   _frenchcodepostaux_civix_civicrm_entityTypes($entityTypes);
 // }
 
-/**
- * Implements hook_civicrm_themes().
- */
-function frenchcodepostaux_civicrm_themes(&$themes) {
-  _frenchcodepostaux_civix_civicrm_themes($themes);
-}
+
 
 // --- Functions below this ship commented out. Uncomment as required. ---
 
@@ -177,7 +79,6 @@ function frenchcodepostaux_civicrm_buildForm($formName, &$form) {
     CRM_Core_Resources::singleton()->addScriptFile('frenchcodepostaux', 'postcodes.js');
   }
 }
-
 
 function frenchcodepostaux_civicrm_alterContent(  &$content, $context, $tplName, &$object ) {
   if ($object instanceof CRM_Contact_Form_Inline_Address) {
